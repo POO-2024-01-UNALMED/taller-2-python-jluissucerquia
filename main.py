e862d73ece1eedff6bcb9eb868a30edd435403b5
jluissucerquia <jsucerquia@unal.edu.co>
2024-02-19 12:10:50 -0500

    :+1:

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 class Auto:
-    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
         self.asientos = asientos
         self.marca = marca
         self.motor = motor 
         self.registro = registro 
-        self.cantidadCreados = cantidadCreados 
 
 
-def cantidadAsientos(self):
-    numAsientos = 0
-    for asiento in self.asientos:
-        if (type(asiento) == Asiento):
-            numAsientos += 1
-    return numAsientos 
-
-def verificarIntegridad(self):
-    if (self.registro == self.motor.registro):
+    def cantidadAsientos(self):
+        numAsientos = 0
         for asiento in self.asientos:
             if (type(asiento) == Asiento):
-                if asiento.registro != self.registro:
-                    return "Las piezas no son originales"
-        return "Auto original"
-    else:
-        return "Las piezas no son originales"
+                numAsientos += 1
+        return numAsientos 
+
+    def verificarIntegridad(self):
+        if (self.registro) == (self.motor.registro):
+            for asiento in self.asientos:
+                if (type(asiento) == (Asiento):
+                    if asiento.registro != self.registro:
+                        return "Las piezas no son originales"
+            return "Auto original"
+        else:
+            return "Las piezas no son originales"
         
 
 class Asiento:
@@ -33,19 +33,21 @@ class Asiento:
         self.precio = precio 
         self.registro = registro 
 
-def cambiarColor(self,color):
-    if color == "rojo" or color =="verde" or color == "amarillo" or color == "negro" or color == "blanco":
-         nuevo_color = color 
+    def cambiarColor(self, color):
+        if (color == "rojo" or color =="verde" or color == "amarillo" or color == "negro" or color == "blanco"):
+            self.color = color 
+        
 
 class Motor:
+
     def __init__(self, numeroCilindros, tipo, registro):
         self.numeroCilindros = numeroCilindros
         self.tipo = tipo
         self.registro = registro
 
-def cambiarRegistro(self, registro):
-    self.registro = registro
+    def cambiarRegistro(self, registro):
+        self.registro = registro
 
-def asignarTipo(self, tipo):
-    if tipo == "electrico" or tipo == "gasolina":
-        self.tipo = tipo
+    def asignarTipo(self, tipo):
+        if (tipo == "electrico" or tipo == "gasolina"):
+            self.tipo = tipo
